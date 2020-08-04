adcaedb190fef7a90c50926d95e054b7df8fdd70
mikatalk <mikatalk@users.noreply.github.com>
2020-08-03 18:01:08 -0700

    update

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-  mounted: () => document.dispatchEvent(new Event("x-app-rendered")),
-}).$mount('#app')
